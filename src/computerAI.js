0a6ea96c35fd6a3f44c826df0d86453a92356a83
sholdahl <sam.holdahl@gmail.com>
2021-01-20 20:55:36 -0600

    improved computer AI so it will only attack positions it has not yet attacked

--- src/computerAI.js
+++ src/computerAI.js
@@ -1,5 +1,5 @@
 const computerAI = {
-  attack: (computer, player) => {
+  getCoords: () => {
     let xAxis = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let yAxis = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J"];
 
@@ -9,8 +9,30 @@ const computerAI = {
 
     let coords = yAxis[randomNum()] + xAxis[randomNum()];
 
-    computer.attack(coords, player);
+    return coords;
   },
-}
+  attack: (computer, player) => {
+    let coords = "";
+    let coordValid = false;
+    let shipHits = [];
+
+    for (let i = 0; i < player.board.ships.length; i++) {
+      const ship = player.board.ships[i];
+      shipHits.push(...ship.hits);
+    }
+
+    while (coordValid !== true) {
+      coords = computerAI.getCoords();
+
+      if (
+        player.board.missedAttacks.find((missedShots) => missedShots === coords) === undefined &&
+        shipHits.find((hit) => hit === coords) === undefined
+      ) {
+        coordValid = true;
+        computer.attack(coords, player);
+      }
+    }
+  },
+};
 
 export default computerAI;
